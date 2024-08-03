cbdd23b3e2eb8677f7589272a48f7493a4c3f141
aniketvishal <aniketvishal03@gmail.com>
2024-08-03 12:03:28 +0530

    - initial Commit

---
+++ android/app/src/main/kotlin/com/aniketvishal/flutter_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aniketvishal.flutter_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Widgets'),
+    );
+  }
+}
+
+/*
+
+1. What is Git and Github?
+
+2. How to create Github Repository? - Done
+
+3. How to attach your Flutter Project with your Github Repository?
+
+4. How to commit you code and push it to Github?
+
+5. How to crate a new Branch on Github?
+
+6. How to create a Merge Request (MR) on Github & approve it.
+
+7. How to Merge Branches?
+
+8. Pro Tip
+Gitflow
+
+*/
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
