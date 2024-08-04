901c1e7a27c9ef2956f6dfa6816196777496d689
aniketvishal <aniketvishal03@gmail.com>
2024-08-04 15:04:16 +0530

    - text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,79 +19,112 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
+        textTheme: const TextTheme(
+          //Body Medium
+          bodyLarge: TextStyle(fontSize: 20, color: Colors.red, fontWeight: FontWeight.w700),
+          bodyMedium: TextStyle(fontSize: 32, color: Colors.red, fontWeight: FontWeight.w700),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Widgets'),
+      home: const MyHomePage(),
     );
   }
 }
 
-/*
-
-1. What is Git and Github?
-
-2. How to create Github Repository? - Done
-
-3. How to attach your Flutter Project with your Github Repository? - Done
-
-4. How to commit you code and push it to Github? - Done
-
-5. How to crate a new Branch on Github? - Done
-
-6. How to create a Merge Request (MR) on Github & approve it. Hello
-
-7. How to Merge Branches?
-
-8. Pro Tip
-Gitflow
-
-*/
-
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter = _counter + 2;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Container(
+              width: 450,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black12)),
+              /*
+
+              Text Widget Structure
+
+              Important Text Widget Properties:
+               - textAlign
+               - textDirection
+               - overflow
+               - textScaleFactor/textScaler,
+               - maxLines
+               - selectionColor
+
+              Other Widgets related to Text Widgets
+               - TextTheme()
+               - TextStyle()
+               - Text.rich() Hello, Aniket
+               - SelectableText()
+
+              Todo: Pro Tip
+              How to use Global Style like a PRO
+
+              */
+
+              // child: SelectableText(
+              //   'Hello Decoders!!'
+              // ),
+
+              // child: Text.rich(
+              //   TextSpan(
+              //     text: 'Hello, ',
+              //     style: TextStyle(
+              //       fontSize: 32,
+              //       color: Colors.green,
+              //       fontWeight: FontWeight.normal
+              //     ),
+              //     children: [
+              //       TextSpan(
+              //         text: 'Aniket!! ',
+              //         style: TextStyle(
+              //           fontSize: 32,
+              //           color: Colors.red,
+              //           fontWeight: FontWeight.bold
+              //         ),
+              //       ),
+              //       TextSpan(
+              //         text: 'How are you?',
+              //         style: TextStyle(
+              //           fontSize: 32,
+              //           color: Colors.black87
+              //         ),
+              //       )
+              //     ]
+              //   )
+              // ),
+
+              child: Text(
+                'This is a text Widget',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: StyleManager.header.copyWith(
+                  color: Colors.red,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class StyleManager{
+  static const TextStyle header = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: Colors.black87
+  );
 }
