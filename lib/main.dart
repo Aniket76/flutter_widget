f92e2fca75c25767dd973332a3f4f48182de51f2
aniketvishal <aniketvishal03@gmail.com>
2024-08-04 15:13:51 +0530

    - set the base project

--- lib/main.dart
+++ lib/main.dart
@@ -16,76 +16,22 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Widgets'),
+      home: const MyHomePage(),
     );
   }
 }
 
-/*
-
-1. What is Git and Github?
-
-2. How to create Github Repository? - Done
-
-3. How to attach your Flutter Project with your Github Repository? - Done
-
-4. How to commit you code and push it to Github? - Done
-
-5. How to crate a new Branch on Github? - Done
-
-6. How to create a Merge Request (MR) on Github & approve it. Hello
-
-7. How to Merge Branches?
-
-8. Pro Tip
-Gitflow
-
-*/
-
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter = _counter + 2;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return const SizedBox();
   }
 }
+
